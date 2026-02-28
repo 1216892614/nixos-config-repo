1df65ec23caa8bc91a90dafa78e5c8a2af1594f4
NerdHu <hndppt@outlook.com>
2026-03-01 00:24:13 +0800

    Update .gitignore to include temporary YAML files and refactor git and user configurations to use environment variables for user details and SSH keys.

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -1,11 +1,15 @@
 { pkgs, ... }:
 
+let
+  env = import ../../env.nix;
+in
 {
   users.users.ep-o1 = {
     isNormalUser = true;
     home = "/home/ep-o1";
     shell = pkgs.fish;
     extraGroups = [ "wheel" "networkmanager" "video" "audio" "docker" ];
+    openssh.authorizedKeys.keys = [ env.sshPublicKey ];
   };
 
   programs.fish.enable = true;
